103abd24c9db5384f59b67778bf8fbc02e766ffa
William CW Lau <contact@cwlau.com>
2016-08-01 01:16:32 +0800

    adding back test_main

---
+++ test_main.py
@@ -0,0 +1,18 @@
+
+import main
+import unittest
+
+class MainTest(unittest.TestCase):
+    """This class uses the Flask tests app to run an integration test against a
+    local instance of the server."""
+
+    def setUp(self):
+        self.app = main.app.test_client()
+
+    def test_hello_world(self):
+        # rv = self.app.get('/get_author/ulysses')
+        text = "Hello world"
+        assert text == "Hello world"
+
+if __name__ == '__main__':
+    unittest.main()
